94be9cc7a87aaafd823c6e3e031d87ca4d4e0154
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2022-12-31 14:33:23 +0530

    Class2 done successfully using bundlers

--- class2/IgniteApp/App.js
+++ class2/IgniteApp/App.js
@@ -1,4 +1,7 @@
-const h1 = React.createElement("h1", {}, "Ignite your react app")
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const h1 = React.createElement("h1", {}, "Ignite your react app - Class 2")
 const p = React.createElement("p", {}, "Bundlers and Package Mangaer - Own Implementation of create-react-app")
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render([h1, p])
